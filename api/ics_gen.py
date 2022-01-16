b06f50edd3e7429e29001aafeb72f00ab3585fa2
jordans6 <jordanschneider@telus.net>
2022-01-16 04:32:25 -0800

    update

--- api/ics_gen.py
+++ api/ics_gen.py
@@ -6,9 +6,9 @@ from pathlib import Path
 
 def create_ics(data):
     #times are in UTC
-    TIMES_START_HOUR = [17, 19, 21, 0, 2, 5]
+    TIMES_START_HOUR = [16, 18, 20, 23, 2, 5]
     TIMES_START_MINUTE = [0, 30, 30, 0, 30, 0]
-    TIMES_END_HOUR = [19, 21, 23, 3, 5, 9]
+    TIMES_END_HOUR = [18, 20, 22, 3, 4, 8]
     TIMES_END_MINUTE = [0, 0, 30, 0, 0, 0]
     ACTIVITIES = ['activity1', 'lunch', 'activity2', 'activity3', 'dinner', 'night']
     DATE = '12/02/2023'
@@ -19,7 +19,10 @@ def create_ics(data):
     for item in data:
         for activ in ACTIVITIES:
             if item[activ] is None:
-                break
+                count += 1
+                if count % 4 == 0:
+                    day += 1
+                continue
             else:
                 details = item[activ]
                 location = details["location"]
@@ -28,13 +31,12 @@ def create_ics(data):
                 event = Event()
                 event.add('summary', name)
                 event.add('location', location)
-                event.add('dtstart', datetime(2022, 10, 12+day, TIMES_START_HOUR[count%6], TIMES_END_MINUTE[count%6], 0, tzinfo=pytz.utc))
-                event.add('dtend', datetime(2022, 10, 12+day, TIMES_END_HOUR[count%6], TIMES_END_MINUTE[count%6], 0, tzinfo=pytz.utc))
-                event.add('dtstamp', datetime(2022, 1, 12+day, 0, 10, 0, tzinfo=pytz.utc))
+                event.add('dtstart', datetime(2022, 5, 7+day, TIMES_START_HOUR[count%6], TIMES_END_MINUTE[count%6], 0, tzinfo=pytz.utc))
+                event.add('dtend', datetime(2022, 5, 7+day, TIMES_END_HOUR[count%6], TIMES_END_MINUTE[count%6], 0, tzinfo=pytz.utc))
                 cal.add_component(event)
                 
                 count += 1
-                if count % 6 == 0:
+                if count % 4 == 0:
                     day += 1
     
 
