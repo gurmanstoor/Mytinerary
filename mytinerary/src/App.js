6bd0dec4d1c57fd83cd1f3b5c4936f6b8b87678f
jordans6 <jordanschneider@telus.net>
2022-01-16 04:34:53 -0800

    Merge in front end code

--- mytinerary/src/App.js
+++ mytinerary/src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
+
+import NavBar from './components/NavBar.js';
+import bg from './assets/Background.png';
 import './App.css';
+import Home from './components/Home';
+import AboutUs from './components/AboutUs';
+import GetStarted from './components/GetStarted.js';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Dates from './components/Dates';
+import Interests from './components/Interests';
+import Busy from './components/Busy';
+import Download from './components/Download';
+
 
 function App() {
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      
+      <BrowserRouter>
+        <NavBar/>
+        <Switch>
+          <Route exact path="/" component = {Home}></Route>
+          <Route exact path="/AboutUs" component={AboutUs}></Route>
+          <Route exact path="/GetStarted" component={GetStarted}></Route>
+          <Route exact path="/Dates" component={Dates}></Route>
+          <Route exact path="/Interests" component={Interests}></Route>
+          <Route exact path="/Busy" component={Busy}></Route>
+          <Route exact path="/Download" component={Download}></Route>
+          
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
